2e9068b2b676bb83a61f8f1d80a86790e8b33632
NguyenTrongNghia1604 <nghiavip1010@gmail.com>
2024-10-17 17:59:01 +0700

    up

--- src/Layout/Componet/User/index.js
+++ src/Layout/Componet/User/index.js
@@ -40,7 +40,7 @@ function User() {
                 <div className={cx('images-user')}>
                     <Images
                         src={
-                            avatar.images !== ''
+                            avatar.images !== ' '
                                 ? `${process.env.REACT_APP_URL_BACKEND}/images/${avatar.images}`
                                 : images.user
                         }
